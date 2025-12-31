d94f5d128531a223a5f2a868eb92a0ad96c28393
Aim asademy <jayant.kgp81@gmail.com>
2025-12-31 20:43:25 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   swMinify: true,
-  disable: false, // process.env.NODE_ENV === "development", // Enable in dev for testing
+  disable: process.env.NODE_ENV === "development",
   workboxOptions: {
     disableDevLogs: true,
   },
