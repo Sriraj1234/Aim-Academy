69dd4b9b589f40c05793013989292f68668d97a1
Aim asademy <jayant.kgp81@gmail.com>
2026-01-03 17:05:29 +0530

    Create page.tsx

---
+++ app/play/snap-solve/page.tsx
@@ -0,0 +1,9 @@
+import SnapSolver from '@/components/ai-solver/SnapSolver';
+
+export default function SnapSolvePage() {
+    return (
+        <div className="min-h-screen bg-gray-50 pt-20">
+            <SnapSolver />
+        </div>
+    );
+}
