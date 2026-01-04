930966d0dc9fe419c6cc556c1585c05431381abd
Aim asademy <jayant.kgp81@gmail.com>
2026-01-04 18:25:51 +0530

    Update page.tsx

--- app/admin/notifications/page.tsx
+++ app/admin/notifications/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import { FaBell, FaPaperPlane, FaUsers, FaCheckCircle, FaTimesCircle, FaInfoCircle } from 'react-icons/fa';
 import toast from 'react-hot-toast';
