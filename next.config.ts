bbb2d52748407bb5043d196f779f51e2d168b986
Aim asademy <jayant.kgp81@gmail.com>
2026-01-05 17:25:11 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   disable: process.env.NODE_ENV === "development",
   workboxOptions: {
     disableDevLogs: true,
+    importScripts: ["/firebase-messaging-sw.js"],
   },
 });
 
