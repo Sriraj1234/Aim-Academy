7bcc742d6fc377b10195ac6e46a63b7298c71f87
Aim asademy <jayant.kgp81@gmail.com>
2026-01-06 16:54:14 +0530

    Delete get_api_key.cjs

--- scripts/get_api_key.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const dotenv = require('dotenv');
-
-const envPath = path.resolve(__dirname, '../.env.local');
-const envConfig = dotenv.parse(fs.readFileSync(envPath));
-
-console.log('FULL_API_KEY:', envConfig.NEXT_PUBLIC_FIREBASE_API_KEY);
