cf37c46622e4422dc614f4cbfe781ef1caec1d01
Aim asademy <jayant.kgp81@gmail.com>
2026-02-07 00:17:01 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,6 +8,9 @@ const config: CapacitorConfig = {
         url: 'https://padhaku.co.in',
         cleartext: true,
         androidScheme: 'https'
+    },
+    android: {
+        overrideUserAgent: "Mozilla/5.0 (Linux; Android 14; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.6167.101 Mobile Safari/537.36"
     }
 };
 
