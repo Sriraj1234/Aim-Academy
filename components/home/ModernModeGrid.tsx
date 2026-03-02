a129ebf917ac27f3113a77189de0e0bd3a7344c7
Padhaku <jayant.kgp81@gmail.com>
2026-03-02 23:45:54 +0530

    Update ModernModeGrid.tsx

--- components/home/ModernModeGrid.tsx
+++ components/home/ModernModeGrid.tsx
@@ -95,7 +95,7 @@ export const ModernModeGrid = () => {
                 </Link>
             </div>
 
-            <div className="grid md:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-5">
                 {modes.map((mode) => (
                     <Link
                         href={
